18f498b91ed69b414eade5ae5ea2c91fd35d3caf
Charly <66905063+EnisuVI@users.noreply.github.com>
2023-11-07 09:16:29 +0100

    Question 1 et 2

--- src/reseau/Point.java
+++ src/reseau/Point.java
@@ -1,4 +1,66 @@
 package reseau;
 
 public class Point {
+    /**
+     * représente l'identifiant unique du client
+     */
+    public int idClient;
+    /**
+     * abscisse à laquelle se situe le client
+     */
+    public float abscisse;
+    /**
+     * ordonnée à laquelle se situe le client
+     */
+    public float ordonnee;
+
+    public Point(float abscisse, float ordonnee) {
+        this.abscisse = abscisse;
+        this.ordonnee = ordonnee;
+    }
+
+    /**
+     * getters
+     */
+    public float getAbscisse() {
+        return abscisse;
+    }
+
+    public float getOrdonnee() {
+        return ordonnee;
+    }
+
+    /**
+     * setters
+     */
+    public void setAbscisse(float abscisse) {
+        this.abscisse = abscisse;
+    }
+
+    public void setOrdonnee(float ordonnee) {
+        this.ordonnee = ordonnee;
+    }
+
+    /**
+     * méthode toString
+     */
+    @Override
+    public String toString() {
+        return "Point{" +
+                "idClient=" + idClient +
+                ", abscisse=" + abscisse +
+                ", ordonnee=" + ordonnee +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Point p1 = new Point(1,3);
+        Point p2 = new Point(3,5);
+        Point p3 = new Point(6, 2);
+        Point p4 = new Point(12, 10);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+    }
 }
